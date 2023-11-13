855f242ee0bcfb43b97b2626ebd5638a1765e5e3
Olusegun <ayosegun2001@gmail.com>
2023-11-13 17:10:55 +0100

    Task 0: I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ _printf.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * _printf - a function that produces output according to a format
+ * @format: a character string composed of zero or more directives
+ *
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	va_list custom_args;
+	int count = 0;
+	char *custom_str;
+
+	va_start(custom_args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			count++;
+		}
+		else
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					count += _putchar(va_arg(custom_args, int));
+					break;
+				case 's':
+					custom_str = va_arg(custom_args, char *);
+					if (custom_str == NULL)
+						custom_str = "(null)";
+					while (*custom_str)
+					{
+						count += _putchar(*custom_str);
+						custom_str++;
+					}
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+			}
+		}
+		format++;
+	}
+	va_end(custom_args);
+	return (count);
+}
